f0963eee8650f4e32d053d4ebc5cf58df8931f50
Shubham Pawar <shubham.d.pawar.3333@gmail.com>
2025-01-19 09:16:33 +0530

    Create setup_jenkins.sh

---
+++ setup_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# For Ubuntu 22.04
+# Intsalling Java
+sudo apt update -y
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+sudo java --version
+
+# Installing Jenkins
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
+# Enabling jenkins service
+sudo service jenkins start
+sudo systemctl enable jenkins
